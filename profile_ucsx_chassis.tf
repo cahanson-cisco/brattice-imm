ccd1224f43aac35905581aecbb126c91da6f9886
Casey Hanson <cahanson@cisco.com>
2023-07-20 17:42:09 -0400

    adding ucs-x chassis profiles

--- profile_ucsx_chassis.tf
+++ profile_ucsx_chassis.tf
@@ -1,28 +1,28 @@
-# data "intersight_equipment_chassis" "brattice" {
-#   device_mo_id = data.intersight_asset_device_registration.brattice.results[0].moid
-# }
+data "intersight_equipment_chassis" "brattice" {
+  device_mo_id = data.intersight_asset_device_registration.brattice.results[0].moid
+}
 
-# resource "intersight_chassis_profile" "brattice-chassis" {
-#   for_each = { for index, chassis in data.intersight_equipment_chassis.brattice.results :
-#     chassis.moid => chassis
-#   }
+resource "intersight_chassis_profile" "brattice-chassis" {
+  for_each = { for index, chassis in data.intersight_equipment_chassis.brattice.results :
+    chassis.moid => chassis
+  }
 
-#   name = each.value.name
-#   tags = [local.terraform]
-#   organization {
-#     moid = local.organization
-#   }
+  name = each.value.name
+  tags = [local.terraform]
+  organization {
+    moid = local.organization
+  }
 
-#   target_platform = "FIAttached"
-#   type            = "instance"
+  target_platform = "FIAttached"
+  type            = "instance"
 
-#   policy_bucket {
-#     moid        = intersight_power_policy.grid_last_state.moid
-#     object_type = intersight_power_policy.grid_last_state.object_type
-#   }
+  policy_bucket {
+    moid        = intersight_power_policy.grid_last_state.moid
+    object_type = intersight_power_policy.grid_last_state.object_type
+  }
 
-#   policy_bucket {
-#     moid        = intersight_thermal_policy.thermal["LowPower"].moid
-#     object_type = intersight_thermal_policy.thermal["LowPower"].object_type
-#   }
-# }
+  policy_bucket {
+    moid        = intersight_thermal_policy.thermal["LowPower"].moid
+    object_type = intersight_thermal_policy.thermal["LowPower"].object_type
+  }
+}
