8b77ccad946f7c646eda552e87510f47397081ed
Casey Hanson <cahanson@cisco.com>
2023-07-24 23:50:35 -0400

    changing esxi management vnic to fabric failover

--- policy_lan_connectivity.tf
+++ policy_lan_connectivity.tf
@@ -9,15 +9,17 @@ resource "intersight_vnic_lan_connectivity_policy" "brattice-esxi" {
   target_platform = "FIAttached"
 }
 
-resource "intersight_vnic_eth_if" "aci-a" {
-  name = "aci-a"
+resource "intersight_vnic_eth_if" "mgmt-failover" {
+  name = "mgmt-failover"
   tags = [local.terraform]
 
   order            = 2
   mac_address_type = "POOL"
+  failover_enabled = "true"
+
 
   mac_pool {
-    moid = intersight_macpool_pool.brattice_mac_a.moid
+    moid = intersight_macpool_pool.brattice_mac_failover.moid
   }
 
   placement {
@@ -54,57 +56,11 @@ resource "intersight_vnic_eth_if" "aci-a" {
   }
 }
 
-resource "intersight_vnic_eth_if" "aci-b" {
-  name = "aci-b"
-  tags = [local.terraform]
-
-  order            = 3
-  mac_address_type = "POOL"
-
-  mac_pool {
-    moid = intersight_macpool_pool.brattice_mac_b.moid
-  }
-
-  placement {
-    switch_id = "B"
-    id        = "MLOM"
-
-  }
-
-  cdn {
-    nr_source = "vnic"
-  }
-
-  fabric_eth_network_group_policy {
-    moid = intersight_fabric_eth_network_group_policy.vlan-1599-native.moid
-  }
-
-  fabric_eth_network_control_policy {
-    moid = intersight_fabric_eth_network_control_policy.cdp_enable.moid
-  }
-
-  eth_qos_policy {
-    moid = intersight_vnic_eth_qos_policy.default["be"].moid
-  }
-
-  eth_adapter_policy {
-    moid = intersight_vnic_eth_adapter_policy.default.moid
-  }
-
-  lifecycle {
-    ignore_changes = [cdn]
-  }
-
-  lan_connectivity_policy {
-    moid = intersight_vnic_lan_connectivity_policy.brattice-esxi.moid
-  }
-}
-
 resource "intersight_vnic_eth_if" "vmm-a" {
   name = "vmm-a"
   tags = [local.terraform]
 
-  order            = 4
+  order            = 3
   mac_address_type = "POOL"
 
   mac_pool {
@@ -149,7 +105,7 @@ resource "intersight_vnic_eth_if" "vmm-b" {
   name = "vmm-b"
   tags = [local.terraform]
 
-  order            = 5
+  order            = 4
   mac_address_type = "POOL"
 
   mac_pool {
