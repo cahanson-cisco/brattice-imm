1a1eaada7ac9c329d6d86c3a891fddee9d561927
Casey Hanson <cahanson@cisco.com>
2023-07-26 17:26:49 -0400

    testing new domain profile policy buckets

--- profile_ucs_domain.tf
+++ profile_ucs_domain.tf
@@ -27,6 +27,46 @@ resource "intersight_fabric_switch_profile" "brattice-6454-B" {
   }
 }
 
+resource "intersight_fabric_switch_cluster_profile" "brattice-6454-pb" {
+  name = "brattice-6454-pb"
+  tags = [local.terraform]
+  organization {
+    moid = local.organization
+  }
+  type = "instance"
+}
+
+resource "intersight_fabric_switch_profile" "brattice-6454-A-pb" {
+  name = "brattice-6454-A-pb"
+  tags = [local.terraform]
+
+  type = "instance"
+
+  policy_bucket { # VLAN Configuration
+    moid        = intersight_fabric_eth_network_policy.brattice-vlans.moid
+    object_type = intersight_fabric_eth_network_policy.brattice-vlans.object_type
+  }
+
+  switch_cluster_profile {
+    moid = intersight_fabric_switch_cluster_profile.brattice-6454-pb.moid
+  }
+}
+
+resource "intersight_fabric_switch_profile" "brattice-6454-B-pb" {
+  name = "brattice-6454-B-pb"
+  tags = [local.terraform]
+  type = "instance"
+
+  policy_bucket { # VLAN Configuration
+    moid        = intersight_fabric_eth_network_policy.brattice-vlans.moid
+    object_type = intersight_fabric_eth_network_policy.brattice-vlans.object_type
+  }
+
+  switch_cluster_profile {
+    moid = intersight_fabric_switch_cluster_profile.brattice-6454-pb.moid
+  }
+}
+
 data "intersight_asset_device_registration" "brattice" {
   device_hostname = ["BRATTICE"]
   platform_type   = "UCSFIISM"
